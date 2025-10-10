f6e0e9195c10d8e3fdb50979d5090d24957eaf68
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-10-10 17:54:26 +0300

    Test Create & Delete.

--- backend/src/services/user/user.test.ts
+++ backend/src/services/user/user.test.ts
@@ -1,13 +1,19 @@
 import { UserModel } from "./user.model";
 import { Gender, Role } from "../../../../src/schemas/UserSchema";
 
-UserModel.create({
-    role: Role.admin,
-    gender: Gender.male,
-    password: "Abc123!@",
-    "first-name": "Hashem",
-    "last-name": "Wannous",
-    birthday: new Date(2003, 2, 15),
-    username: "@ashamethedestroyer",
-    email: "hashem.wannous@gmail.com",
-});
+async function Test() {
+    const { _id } = await UserModel.create({
+        role: Role.admin,
+        gender: Gender.male,
+        "last-name": "User",
+        "first-name": "Test",
+        password: "Abc123!@",
+        username: "@testuser",
+        email: "test.user@gmail.com",
+        birthday: new Date(2003, 2, 15),
+    });
+
+    await UserModel.deleteOne({ _id });
+}
+
+Test();
