e83f82fa50b08a7b2aa00c978ca9f4b8bc1dc699
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-10-10 19:42:18 +0300

    CRUDding on User.

--- backend/src/services/user/user.routes.ts
+++ backend/src/services/user/user.routes.ts
@@ -1,12 +1,66 @@
 import { Router } from "express";
-import { GetUsers } from "./user.services";
+import { Role } from "../../../../src/schemas/UserSchema";
+import {
+    ValidateRateLimit,
+    ValidateAuthority,
+    ValidateAuthenticity,
+} from "src/middlewares";
+import {
+    GetUsers,
+    PostUser,
+    PatchUser,
+    DeleteUser,
+    GetUserByID,
+    DeleteAllUsers,
+} from "./user.services";
 
 export const USER_ROUTE = "/users";
 export const UserRouter = Router();
 
 UserRouter.get(
     USER_ROUTE,
-    // ValidateAuthenticity,
-    // ValidateRateLimit(),
+    ValidateAuthenticity,
+    ValidateAuthority(Role.admin),
+    ValidateRateLimit({ checkRole: true }),
     GetUsers
+);
+
+UserRouter.get(
+    `${USER_ROUTE}/:id`,
+    ValidateAuthenticity,
+    ValidateAuthority(Role.admin),
+    ValidateRateLimit({ checkRole: true }),
+    GetUserByID
+);
+
+UserRouter.post(
+    USER_ROUTE,
+    ValidateAuthenticity,
+    ValidateAuthority(Role.admin),
+    ValidateRateLimit({ checkRole: true }),
+    PostUser
+);
+
+UserRouter.patch(
+    `${USER_ROUTE}/:id`,
+    ValidateAuthenticity,
+    ValidateAuthority(Role.admin),
+    ValidateRateLimit({ checkRole: true }),
+    PatchUser
+);
+
+UserRouter.delete(
+    USER_ROUTE,
+    ValidateAuthenticity,
+    ValidateAuthority(Role.admin),
+    ValidateRateLimit({ checkRole: true }),
+    DeleteAllUsers
+);
+
+UserRouter.delete(
+    `${USER_ROUTE}/:id`,
+    ValidateAuthenticity,
+    ValidateAuthority(Role.admin),
+    ValidateRateLimit({ checkRole: true }),
+    DeleteUser
 );
--- backend/src/services/user/user.services.ts
+++ backend/src/services/user/user.services.ts
@@ -1,11 +1,33 @@
 import { UserModel } from "./user.model";
-import { GetDocuments } from "src/utils";
-import { RequestHandler } from "express";
+import {
+    GetDocuments,
+    PostDocument,
+    PatchDocument,
+    GetDocumentByID,
+    DeleteDocumentByID,
+    DeleteAllDocuments,
+} from "src/utils";
 
-export const GetUsers: RequestHandler = GetDocuments(
+export const GetUsers = GetDocuments(
     UserModel,
     "-password",
     "-_loginToken",
     "-_resetToken",
     "-_resetTokenExpirationDate"
 );
+
+export const GetUserByID = GetDocumentByID(
+    UserModel,
+    "-password",
+    "-_loginToken",
+    "-_resetToken",
+    "-_resetTokenExpirationDate"
+);
+
+export const PostUser = PostDocument(UserModel);
+
+export const PatchUser = PatchDocument(UserModel);
+
+export const DeleteUser = DeleteDocumentByID(UserModel);
+
+export const DeleteAllUsers = DeleteAllDocuments(UserModel);
