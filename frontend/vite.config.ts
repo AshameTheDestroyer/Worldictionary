24af4b9ee194feaf2d94bc9a8402c92d3633f7a7
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-10-11 03:18:12 +0300

    TailwindCSS Setup.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
     plugins: [
         react(),
+        tailwindcss(),
         tanstackRouter({
             target: "react",
             autoCodeSplitting: true,
         }),
     ],
+    resolve: {
+        alias: {
+            "@": "/src",
+        },
+    },
 });
