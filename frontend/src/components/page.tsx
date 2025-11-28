a2e2b3538832a7aefc7c799cc2331006c3292345
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-11-29 01:41:32 +0300

    Increased Padding on Mobile.

--- frontend/src/components/page.tsx
+++ frontend/src/components/page.tsx
@@ -12,7 +12,7 @@ export const Page: FC<PageProps> = ({ id, children, className }) => {
         <main
             id={id}
             className={cn(
-                "flex-1 flex flex-col gap-16 max-sm:gap-8 p-6 max-md:p-4 max-sm:p-2 -m-8",
+                "flex-1 flex flex-col gap-16 max-sm:gap-8 p-6 max-md:p-4 -m-8",
                 className
             )}
         >
