678ff5e985b7131e7624849854553db63e3762c0
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-11-29 01:42:32 +0300

    useGetUserByUsername Query.

---
+++ frontend/src/services/user/useGetUserByUsername.ts
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import { HTTPManager } from "@/managers/HTTPManager";
+import { UserSchema } from "../../../../src/schemas/UserSchema";
+
+export const GET_USER_BY_USERNAME_KEY = "GET-USER-BY-USERNAME";
+
+export const useGetUserByUsername = (username: string) =>
+    useQuery({
+        queryKey: [GET_USER_BY_USERNAME_KEY, username],
+        queryFn: () =>
+            HTTPManager.get(`/users/${username.replace(/^@/, "")}`)
+                .then((response) => response.data)
+                .then(UserSchema.parse),
+    });
