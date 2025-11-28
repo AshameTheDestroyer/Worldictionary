21b352d587161b1045b10ffe25f1d81b28bcd71c
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-11-29 01:41:53 +0300

    Home Emptied.

--- frontend/src/routes/home.tsx
+++ frontend/src/routes/home.tsx
@@ -1,18 +1,10 @@
-import { PlusIcon } from "lucide-react";
 import { Page } from "@/components/page";
 import { createFileRoute } from "@tanstack/react-router";
-import { ActionButton } from "@/components/action-button";
 
 export const Route = createFileRoute("/home")({
     component: Index,
 });
 
 function Index() {
-    return (
-        <Page>
-            <ActionButton>
-                <PlusIcon className="stroke-3" />
-            </ActionButton>
-        </Page>
-    );
+    return <Page id="home-page">Welcome to "/home"!</Page>;
 }
