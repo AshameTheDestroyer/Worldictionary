f68a494aa57f6697f5223d93e5d64168f0966006
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-11-29 03:27:32 +0300

    Options Provided.

--- frontend/src/services/user/useGetUserByUsername.ts
+++ frontend/src/services/user/useGetUserByUsername.ts
@@ -1,11 +1,15 @@
-import { useQuery } from "@tanstack/react-query";
 import { HTTPManager } from "@/managers/HTTPManager";
+import { useQuery, UseQueryOptions } from "@tanstack/react-query";
 import { UserWithoutPasswordSchema } from "../../../../src/schemas/UserSchema";
 
 export const GET_USER_BY_USERNAME_KEY = "GET-USER-BY-USERNAME";
 
-export const useGetUserByUsername = (username: string) =>
+export const useGetUserByUsername = (
+    username: string,
+    options?: Partial<UseQueryOptions>
+) =>
     useQuery({
+        ...options,
         queryKey: [GET_USER_BY_USERNAME_KEY, username],
         queryFn: () =>
             HTTPManager.get(`/users/${username.replace(/^@/, "")}`)
